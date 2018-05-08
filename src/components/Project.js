3cdc18f68a367814a7e2ca830d592b67ea5e7583
mscott2757 <mscott2757@berkeley.edu>
2018-05-08 15:20:48 -0700

    added support for background images on projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Project = ({ title, imgSrc, body, category }) => {
+  let bannerImg = null;
+  if (imgSrc) {
+    bannerImg = (
+      <div className='project-banner__img-wrapper'>
+        <img src={imgSrc} />
+      </div>
+    );
+  }
   return (
     <div className='project'>
       <div className='project-banner'>
+        {bannerImg}
         <div className='project-banner__title'>
           <a href={`/${category.id}`}>
             <h3 className='project-banner__category'>{category.title}</h3>

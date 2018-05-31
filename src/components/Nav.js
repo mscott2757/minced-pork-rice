175a4a1ef0de17d40272d47bf8c843fe7395603e
mscott2757 <mscott2757@berkeley.edu>
2018-05-30 19:41:55 -0700

    fixed padding and moved resume link in nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { NavIconLink } from './utils';
 
 const Nav = ({ handleToggleResume, icons, links }) => {
+  const toggleResume = (e) => {
+    e.preventDefault();
+    handleToggleResume();
+  }
   return (
     <div className='nav'>
       <ul>
+        <li>
+          <a onClick={toggleResume} href='/toggle-resume' className='nav__link'>resume</a>
+        </li>
         {links.map(({className, path, text }) => {
-          return <li key={path}>
-            <a className={className} href={path}>{text}</a>
-          </li>;
+          return (
+            <li key={text}>
+              <a className={className} href={path}>{text}</a>
+            </li>
+          );
         })}
       </ul>
       <div className='nav__social'>
         {icons.map((iconProps, index) => <NavIconLink {...iconProps} key={index} />)}
-        <div className='nav__icon-container' onClick={handleToggleResume}>
-          <i className={`fa fa-paperclip fa-lg`}></i>
-        </div>
       </div>
     </div>
   );
--- src/content/index.js
+++ src/content/index.js
@@ -16,11 +16,11 @@ const content = {
   nav: {
     icons: [
       { icon: 'linkedin', path: 'https://www.linkedin.com/in/waverlychao/' },
-      // { icon: 'instagram', path: 'https://www.instagram.com/weekendstate/' },
+      { icon: 'instagram', path: 'https://www.instagram.com/weekendstate/' },
     ],
     links: [
-      { className: 'nav__link', text: 'about', path: '/about' },
       { className: 'nav__link', text: 'projects', path: '/projects' },
+      { className: 'nav__link', text: 'about', path: '/about' },
       { className: 'nav__logo', text: 'wc', path: '/' },
     ]
   }

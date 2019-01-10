4882be974ee61b8f6ab587c5b1ba2d7e0134a31b
Waverly Chao <waverlychao@gmail.com>
2019-01-09 22:52:33 -0800

    hgc edit

--- src/content/projects/hungryGhostsClub.js
+++ src/content/projects/hungryGhostsClub.js
@@ -4,6 +4,6 @@ import { ProjectImg, Quote, Video } from '../utils';
 export const hungryGhostsClub= {
   thumbImg: 'hgc',
   title: 'Hungry Ghosts Club (餓鬼會)',
-  description: 'Throw in 4 working post-grads, a sense of listlessness, a sprinkle of nostalgia, and a splash of creativity and you get the Hungry Ghosts Club zine. Volume 1, "Views from the 626," explores themes of home, culture, and identity. Digital copy now on Issuu, physical print available for purchase.',
+  description: 'Throw in 4 working post-grads, a sprinkle of nostalgia, and a splash of creativity and you get the Hungry Ghosts Club zine. Volume 1, "Views from the 626," explores themes of home, culture, and identity. Digital copy now on Issuu, physical print available for purchase.',
   externalLink: 'https://issuu.com/hungryghostsclub/docs/hgc-v1-winter2018'
 }

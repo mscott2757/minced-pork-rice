b7f621a3fa2ecf79b549b35c2df824f836dc58ed
Waverly Chao <waverlychao@gmail.com>
2019-03-25 16:47:27 -0700

    updated resume march 25

--- src/content/about.js
+++ src/content/about.js
@@ -8,9 +8,7 @@ export const about = {
       <h3>Hey there, I'm Waverly!</h3>
       <p>I am a strategy consultant by trade, a creative DIY-er/maker at heart, and ultimately, a human being on a mission to transform this world into a better place. </p>
 
-      <p>I believe that it is possible to create a net-positive impact in this world when we start thinking, designing, and building with people in mind. This might be the only way to truly address society’s wicked problems. </p>
-
-      <p>But there is a gap between doing what is easy, financially beneficial, and socially just. </p>
+      <p>I believe that it is possible to create a net-positive impact in this world when we design, build, and think about problems with humanity's best interests in mind. Maybe that's the only way we can truly address society’s wicked problems. </p>
 
       <p>Through studying the drivers of behavioral change, decision making processes, and identity formation from the lenses of media consumption, culture, human interactions, and digital experiences, I draw insights on how we can create holistic, personable, human-dignifying experiences, products, and services. </p>
 
